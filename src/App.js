cea98d3b839ae73869e7f8070c310c1ccbf3be4b
Abdify <abdullah.fahimm08@gmail.com>
2021-02-25 20:46:53 +0600

    folder and files done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <Folder name="Desktop">
+              <Folder name="Books">
+                  <File name="The Great Coder.pdf" />
+                  <File name="Multivariable Calculus.pdf" />
+              </Folder>
+              <File name="flower.jpeg" />
+              <File name="phone.png" />
+          </Folder>
+          <Folder name="Application" />
+      </div>
   );
+
+  function Folder(props) {
+    const {name, children} = props;
+    const [isOpen, setIsOpen] = useState(false);
+    const direction = isOpen ? 'down' : 'right'
+
+    return (
+        <div style={{ backgroundColor: "lightgray", marginTop: "10px", padding: "10px 0", cursor: 'pointer' }}>
+            <div style={{ fontWeight: "bold" }} onClick={() => setIsOpen(!isOpen)}>
+                <i className={`angle double ${direction} icon`}></i>
+                {name}
+            </div>
+
+            <div style={{ marginLeft: "20px" }}>{isOpen ? children : null}</div>
+        </div>
+    );
+  }
+
+  function File(props) {
+    const {name} = props;
+    const fileExtension = name.split('.')[1];
+    console.log(fileExtension);
+    const fileIcons = {
+      mp4: 'headphone',
+      png: 'file image outline',
+      jpeg: 'file image',
+      pdf: 'book'
+    }
+    return (
+        <div>
+            <i className={`${fileIcons[fileExtension]} icon`}></i>
+            {name}
+        </div>
+    );
+  }
+
+
 }
 
 export default App;
